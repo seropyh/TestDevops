20d3d697db304aae5f13f7d6c689ed014cab60dd
osv <osv@localhost.localdomain>
2024-07-09 21:38:47 +0300

    install cuber and helm

--- depl.groovy
+++ depl.groovy
@@ -1,5 +1,5 @@
 pipeline {
-    agent  any
+/*    agent  any
 
     stages {
         stage('Подготовка') {
@@ -25,4 +25,36 @@ pipeline {
             }
         }
     }
-}
+    */
+    
+stages {
+        stage("deploy") {
+            agent {
+                kubernetes {
+                    cloud 'kubernetes'
+                    label 'cd'
+                    yamlFile 'jenkins/python-cd.yaml'
+                }
+            }
+
+            steps {
+                container('cd') {
+                    dir("helm") {
+                        sh "echo 'Simple kubectl install!'"
+                        sh "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl"
+                        sh "chmod +x ./kubectl"
+                        sh "mv ./kubectl /usr/local/bin/kubectl"
+
+                        sh "echo 'Simple helm install!'"
+                        sh "wget https://get.helm.sh/helm-v3.2.4-linux-amd64.tar.gz"
+                        sh "tar zxfv helm-v3.2.4-linux-amd64.tar.gz"
+                        sh "cp linux-amd64/helm ."
+
+                        sh "echo 'upgrade app!'"
+                        sh "./helm upgrade --install --wait app ./app"
+                    }
+                }
+            }
+        }
+    }
+}    

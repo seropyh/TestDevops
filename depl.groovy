163d8221c8d1c184de76a67a8b087d1d1b4dd987
osv <osv@localhost.localdomain>
2024-07-11 22:44:36 +0300

    deploy

--- depl.groovy
+++ depl.groovy
@@ -26,44 +26,33 @@
         }
     }
     */
-agent any  
-
-pipeline {    
-
-stages {
+pipeline {
+    agent any
+    stages {
         stage("deploy") {
-	agent {
+            agent {
                 kubernetes {
                     cloud 'kubernetes'
-
-                    label 'jenkins'
-                }
-            }	
                     label 'cd'
-                    sh "echo Hello helm"
-                }    
+                    yamlFile 'jenkins/python-cd.yaml'
+                }
             }
 
-
-        
             steps {
-
-		    script {
-		sh "echo Hello world"	
-		}
-                container('jenkins') {
-                
-                container('nsnginx') {
-
+                container('cd') {
                     dir("helm") {
-                        
+                        sh "echo 'Simple kubectl install!'"
+                        sh "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl"
+                        sh "chmod +x ./kubectl"
+                        sh "mv ./kubectl /usr/local/bin/kubectl"
+
                         sh "echo 'Simple helm install!'"
                         sh "wget https://get.helm.sh/helm-v3.2.4-linux-amd64.tar.gz"
                         sh "tar zxfv helm-v3.2.4-linux-amd64.tar.gz"
                         sh "cp linux-amd64/helm ."
 
                         sh "echo 'upgrade app!'"
-                        sh "./helm upgrade --instaODll --wait app ./app"
+                        sh "./helm upgrade --install --wait app ./app"
                     }
                 }
             }

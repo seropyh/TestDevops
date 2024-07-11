0815d4946c66beab22333b13aa0a0132417c4f98
osv <osv@localhost.localdomain>
2024-07-11 23:10:19 +0300

    deploy

--- depl.groovy
+++ depl.groovy
@@ -41,11 +41,6 @@ pipeline {
             steps {
                 container('jenkins') {
                     dir("helm") {
-                        sh "echo 'Simple kubectl install!'"
-                        sh "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl"
-                        sh "chmod +x ./kubectl"
-                        sh "mv ./kubectl /usr/local/bin/kubectl"
-
                         sh "echo 'Simple helm install!'"
                         sh "wget https://get.helm.sh/helm-v3.2.4-linux-amd64.tar.gz"
                         sh "tar zxfv helm-v3.2.4-linux-amd64.tar.gz"

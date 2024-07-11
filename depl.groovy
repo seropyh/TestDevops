fc7155044fac65cacadef78b305a876ef577e149
osv <osv@localhost.localdomain>
2024-07-11 20:38:25 +0300

    commin

--- depl.groovy
+++ depl.groovy
@@ -1,4 +1,4 @@
-pipeline {
+
 /*    agent  any
 
     stages {
@@ -26,34 +26,46 @@ pipeline {
         }
     }
     */
+<<<<<<< HEAD
 agent any  
+=======
+pipeline {    
+>>>>>>> b30914d (install helm pod)
 stages {
         stage("deploy") {
 	agent {
                 kubernetes {
                     cloud 'kubernetes'
+<<<<<<< HEAD
                     label 'jenkins'
                 }
             }	
+=======
+                    label 'cd'
+                    sh "echo Hello helm"
+                }    
+            }
+>>>>>>> b30914d (install helm pod)
 
+        
             steps {
+<<<<<<< HEAD
 		    script {
 		sh "echo Hello world"	
 		}
                 container('jenkins') {
+=======
+                container('nsnginx') {
+>>>>>>> b30914d (install helm pod)
                     dir("helm") {
-                        sh "echo 'Simple kubectl install!'"
-                        sh "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl"
-                        sh "chmod +x ./kubectl"
-                        sh "mv ./kubectl /usr/local/bin/kubectl"
-
+                        
                         sh "echo 'Simple helm install!'"
                         sh "wget https://get.helm.sh/helm-v3.2.4-linux-amd64.tar.gz"
                         sh "tar zxfv helm-v3.2.4-linux-amd64.tar.gz"
                         sh "cp linux-amd64/helm ."
 
                         sh "echo 'upgrade app!'"
-                        sh "./helm upgrade --install --wait app ./app"
+                        sh "./helm upgrade --instaODll --wait app ./app"
                     }
                 }
             }

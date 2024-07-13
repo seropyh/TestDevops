19e9cc05e39e79900d0f412a41d738c106ddfe2b
osv <osv@localhost.localdomain>
2024-07-13 18:05:20 +0300

    deploy

--- depl.groovy
+++ depl.groovy
@@ -1,12 +1,12 @@
-pipeline {
+pipeline{
     agent any
     stages {
         stage ("install helm"){
         steps {
         script {
-        sh "curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3"
-        sh "chmod 700 get_helm.sh"
-        sh"./get_helm.sh"
+        sh "ls -la"
+        sh "chmod 777 ./helm"
+        sh "./helm"
         }   }   }
         stage("deploy") {
             agent {

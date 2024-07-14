b4134dd4ac9e502e4d332869d13f118b0408b175
osv <osv@localhost.localdomain>
2024-07-14 22:51:03 +0300

    deploy

--- depl.groovy
+++ depl.groovy
@@ -6,27 +6,33 @@ pipeline{
         script {
         sh "ls -la"
         sh "chmod 777 ./helm"
+        sh "./helm"
         }   }   }
         stage("deploy") {
             agent {
                 kubernetes {
                     cloud 'kubernetes'
-                    label 'jenkins'   
+                    label 'nginx'   
                   }               }
             steps {
 
             container('jnlp'){
-                    dir("helm") {
-                        sh "echo 'Simple helm install!'"
+                //    dir("helm"){
+                        sh "./helm install  nginx  ./nginx"
+                    
+                    /*    sh "echo 'Simple helm install!'"
                         sh "wget https://get.helm.sh/helm-v3.2.4-linux-amd64.tar.gz"
                         sh "tar zxfv helm-v3.2.4-linux-amd64.tar.gz"
 	                sh "cp linux-amd64/helm ."
                         sh "./helm repo list"
                         sh "ls -la"
-                         
+    sh "./helm install  nginx  ./nginx"                         
                         sh "echo 'upgrade app!'"
                         sh "./helm upgrade 01--install --wait app ./app" 
-                }
+                         
+            */   
+
+        //     }
             }
         }    }    }
  }

93a11037edf721a50dafb362bff09d7564865f9e
osv <osv@localhost.localdomain>
2024-07-16 23:01:28 +0300

    deploy nginx

--- depl.groovy
+++ depl.groovy
@@ -4,6 +4,7 @@ pipeline{
     
     stage('Remote SSH') {
       steps {
+	step{     
       def remote = [:]
 	remote.name = 'test'
 	remote.host = '192.168.49.1'
@@ -11,17 +12,13 @@ pipeline{
 	remote.password = 'osv'
 	remote.allowAnyHosts = true
 	remote.fileTransfer = 'scp'
-          }}
-    stage ('copy chart to remote server'){
-      steps {
+          
       sshPut remote: remote, from: './nginx/nginx/', into: '~/tchart/'
-    }}
-    stage ('install  chart nginx to remote server'){
-     steps {
       sshCommand remote: remote, command: "helm install -n  nsnginx nginx01 /home/osv/tchart/nginx/"
       sshRemove remote: remote, path: "/home/osv/tchart/nginx/"
+}
+}
 }    
 }
 
-}
 }

99f74345abb26959345578c20e0540b392b41b3c
osv <osv@localhost.localdomain>
2024-07-17 00:15:30 +0300

    deploy nginx

--- depl.groovy
+++ depl.groovy
@@ -13,10 +13,6 @@ def remote = [:]
     remote.allowAnyHosts = true
     remote.fileTransfer = 'scp'
     stage('Remote SSH') {
-      sshCommand remote: remote, command: "ls -lrt"
-      sshCommand remote: remote, command: "for i in {1..5}; do echo -n \"Loop \$i \"; date ; sleep 1; done"
-    }
-    stage ('copy chart to remote server'){
       sshPut remote: remote, from: './nginx/nginx/', into: '~/tchart/'
     }
     stage ('install  chart nginx to remote server'){

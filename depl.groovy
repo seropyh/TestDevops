b1bb68af5b56b9926734527874b63bc568ce8d24
osv <osv@localhost.localdomain>
2024-07-16 22:35:10 +0300

    deploy nginx

--- depl.groovy
+++ depl.groovy
@@ -9,6 +9,7 @@
 pipeline{
     agent any
     stages {
+    steps {
     stage('Remote SSH') {
       sshCommand remote: remote, command: "ls -lrt"
       sshCommand remote: remote, command: "for i in {1..5}; do echo -n \"Loop \$i \"; date ; sleep 1; done"
@@ -23,4 +24,5 @@ pipeline{
     }
 }
 
+}
 }

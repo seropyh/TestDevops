3fadb0d732dd5b48aef8d3aba444e7ae4cf6cd4e
osv <osv@localhost.localdomain>
2024-07-16 15:58:53 +0300

    deploy

--- depl.groovy
+++ depl.groovy
@@ -12,7 +12,7 @@ def remote = [:]
     remote.user = 'osv'
     remote.password = 'osv'
     remote.allowAnyHosts = true
-    remote.fileTransfer = 'SCP'
+    //`remote.fileTransfer = 'SCP'
     stage('Remote SSH') {
       sshCommand remote: remote, command: "ls -lrt"
       sshCommand remote: remote, command: "for i in {1..5}; do echo -n \"Loop \$i \"; date ; sleep 1; done"

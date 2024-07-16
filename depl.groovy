f0a7c779b3c67b2dbf6ff8fc9c3ea3556c6c10dc
osv <osv@localhost.localdomain>
2024-07-16 22:58:51 +0300

    deploy nginx

--- depl.groovy
+++ depl.groovy
@@ -1,20 +1,17 @@
-        def remote = [:]
-	remote.name = 'test'
-	remote.host = '192.168.49.1'
-	remote.user = 'osv'
-	remote.password = 'osv'
-	remote.allowAnyHosts = true
-	remote.fileTransfer = 'scp'
-
 pipeline{
     agent any
     stages {
     
     stage('Remote SSH') {
       steps {
-      sshCommand remote: remote, command: "ls -lrt"
-      sshCommand remote: remote, command: "for i in {1..5}; do echo -n \"Loop \$i \"; date ; sleep 1; done"
-    }}
+      def remote = [:]
+	remote.name = 'test'
+	remote.host = '192.168.49.1'
+	remote.user = 'osv'
+	remote.password = 'osv'
+	remote.allowAnyHosts = true
+	remote.fileTransfer = 'scp'
+          }}
     stage ('copy chart to remote server'){
       steps {
       sshPut remote: remote, from: './nginx/nginx/', into: '~/tchart/'

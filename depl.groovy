996a75bc60ab338852b334718a002ff5910913c4
osv <osv@localhost.localdomain>
2024-07-16 22:38:54 +0300

    deploy nginx

--- depl.groovy
+++ depl.groovy
@@ -16,13 +16,14 @@ pipeline{
       sshCommand remote: remote, command: "for i in {1..5}; do echo -n \"Loop \$i \"; date ; sleep 1; done"
     }}
     stage ('copy chart to remote server'){
+      steps {
       sshPut remote: remote, from: './nginx/nginx/', into: '~/tchart/'
-    }
+    }}
     stage ('install  chart nginx to remote server'){
-
+     steps {
       sshCommand remote: remote, command: "helm install -n  nsnginx nginx01 /home/osv/tchart/nginx/"
       sshRemove remote: remote, path: "/home/osv/tchart/nginx/"
-    
+}    
 }
 
 }
